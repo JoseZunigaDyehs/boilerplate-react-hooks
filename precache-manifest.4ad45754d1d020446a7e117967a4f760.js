c37e749d7b691bd1ac0e236d267f4807cbe91964
Jose Zuñiga <josezuniga@Joses-MacBook-Pro.local>
2019-04-22 12:13:56 -0300

    Updates

---
+++ precache-manifest.4ad45754d1d020446a7e117967a4f760.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebbfaff26ae2ad060fa9",
+    "url": "/static/js/main.852395f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22a0e590f600b7598fbb",
+    "url": "/static/js/2.8b94e47a.chunk.js"
+  },
+  {
+    "revision": "021049e41c74e95d7c58f628efc97f99",
+    "url": "/index.html"
+  }
+];
